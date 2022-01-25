51ecc13518af3d95a18ccf2d41f23e53fad48432
Jacob Zimmerman <d00425031@dmail.dixie.edu>
2022-01-24 22:07:39 -0700

    Polishing prompts

--- src/user_io.cpp
+++ src/user_io.cpp
@@ -4,29 +4,29 @@
 
 std::string getString( std::istream& is, std::ostream& os, const std::string& prompt) {
     std::string x;
-    os << prompt << std::endl;
+    os << prompt;
     is >> x;
     return x;
 }
 
 int getInteger( std::istream& is, std::ostream& os, const std::string& prompt) {
     int x;
-    os << prompt << std::endl;
+    os << prompt;
     is >> x;
     return x;
 }
 
 double getDouble( std::istream& is, std::ostream& os, const std::string& prompt ) {
     double x;
-    os << prompt << std::endl;
+    os << prompt;
     is >> x;
     return x;
 }
 
 int askQuestions3(std::istream& is, std::ostream& os) {
-    std::string p1 = "What is your favorite color?";
-    std::string p2 = "What is your favorite integer?";
-    std::string p3 = "What is your favorite number?";
+    std::string p1 = "What is your favorite color? ";
+    std::string p2 = "What is your favorite integer? ";
+    std::string p3 = "What is your favorite number? ";
 
     std::string color;
     color = getString(is, os, p1);
