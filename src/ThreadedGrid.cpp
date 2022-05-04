380b0d12b6fe555dcd959ae5a3da0ea766fb1b69
Jacob Zimmerman <d00425031@dmail.dixie.edu>
2022-05-03 20:44:20 -0600

    Threaded Grid tests pass

--- src/ThreadedGrid.cpp
+++ src/ThreadedGrid.cpp
@@ -2,9 +2,10 @@
 
 
 
-ThreadedGrid::ThreadedGrid(): NumberGrid() {}
+ThreadedGrid::ThreadedGrid(): NumberGrid(), m_task_list() {
+}
 ThreadedGrid::ThreadedGrid(const int& height, const int& width):
-NumberGrid(height, width) {}
+NumberGrid(height, width),  m_task_list() {}
 ThreadedGrid::~ThreadedGrid() {}
 
 
@@ -13,6 +14,9 @@ void ThreadedGrid::calculateAllNumbers() {
 
     unsigned int max_cores = std::thread::hardware_concurrency();
     unsigned int i;
+    for (int j = 0; j < mHeight * mWidth; j++ ) {
+        m_task_list.push_back(j);
+    }
 
     for(i = 0; i < max_cores-1; i++) {
         threads.push_back(std::thread(&ThreadedGrid::worker, this));
@@ -30,12 +34,11 @@ void ThreadedGrid::worker() {
         //this_workers_primes.resize(0);
         this_workers_tasks.clear();
         m_task_list.pop_back(this_workers_tasks, 1);
-        
-        for (int i = 0; i < mWidth; i++ ) {
-            for (int j = 0; j < mHeight; j++ ) {
-                //setNumber(j, i, calculateNumber( j, i));
-                setNumber(this_workers_tasks[0], j, calculateNumber(this_workers_tasks[0], j));
-            }
+        int t;
+        for (unsigned int i = 0; i < this_workers_tasks.size(); i++ ) {
+            //setNumber(j, i, calculateNumber( j, i));
+            t = this_workers_tasks[i];
+            setNumber((t / mWidth), t % mWidth, calculateNumber((t / mWidth) , t % mWidth));
         }
 
         //m_primes.push_back(this_workers_primes);
